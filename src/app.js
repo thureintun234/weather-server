364212dfcd4f77f669b8ce6adde9baa043b41f20
thureintun234 <thurein12456@gmail.com>
2021-08-25 09:12:16 +0630

    config for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirectory = path.join(__dirname, '../public')
